f051709f1491f2efa34ee9cb39d80aeadd0440f7
SADAT <sadat.himel@gmail.com>
2020-04-22 20:03:51 +0600

    add some DOM files

---
+++ dom2/app.js
@@ -0,0 +1,83 @@
+// const ulHeading = document.getElementById('ul-title');
+
+// ulHeading.style.color="red";
+// console.log(ulHeading);
+
+// const para = document.querySelector('.paragraph');
+
+// para.textContent= "hello";
+// console.log(para);
+
+// const para = document.getElementsByTagName('p');
+
+// for (let i = 0; i<para.length; i++){
+//   para[i].style.color = 'red';
+// }
+
+// const listItem = document.getElementsByClassName('single-item');
+
+// console.log(listItem[3]);
+
+// const para = document.querySelectorAll('p');
+// para.forEach( p => {
+//   p.style.color = 'red';
+//   p.style.background= 'black';
+//   p.style.padding= '10px';
+//   p.style.border= "5px solid green";
+//   p.style.margin = '10px';
+
+// });
+
+// console.log(para);
+
+// const lists = document.querySelectorAll('li');
+// lists.forEach( li => {
+//   li.style.color = 'black';
+//   li.style.background = 'green';
+//   li.style.padding= '10px';
+//   li.style.border= "5px solid skyblue";
+//   li.style.margin = '10px';
+
+// });
+
+// console.log(lists);
+
+
+// const list = document.querySelector('ul#list');
+// console.log(list.childNodes[3])
+
+// const list = document.querySelector('ul#list')
+// console.log(list.children)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
